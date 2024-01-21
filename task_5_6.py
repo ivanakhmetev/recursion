08b15afc94e5e6dd69761c6060f077210759c6cc
ivanakhmetev <78900410+ivanakhmetev@users.noreply.github.com>
2024-01-21 16:36:19 +0300

    Update task_5_6.py

--- task_5_6.py
+++ task_5_6.py
@@ -8,18 +8,15 @@ def recursive_print_even_el(arg: list, start: int):
     if start < len(arg):
         recursive_print_even_el(arg, new_start)
 
-recursive_print_even_el([1,2,3,4,5,6], 0)
+recursive_print_even_el([1,2,3,4,5,6], 10)
 
 def recursive_print_even_idx(arg: list, start: int):
     '''print at even idx'''
-    if start >= len(arg):
-        return None
-    if start % 2 == 0:
+    if start % 2 == 0 and start < len(arg):
         print(arg[start])
     new_start = start + 1
     if start < len(arg) - 1:
         recursive_print_even_idx(arg, new_start)
-    return None
 
 print(recursive_print_even_idx([0,1,0,2,0,3], 8))
     
