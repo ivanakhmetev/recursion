45109c51c5d6215eede3f6a547e1a65a82db9a4a
ivanakhmetev <78900410+ivanakhmetev@users.noreply.github.com>
2024-01-22 20:24:02 +0300

    Add files via upload

---
+++ task_7.py
@@ -0,0 +1,26 @@
+# recursion task 7
+
+def _recursive_find_2nd_largest(arg, max_1, max_2):
+    if len(arg) == 0:
+        return max_2
+    current = arg[0]
+    if current >= max_1:
+        max_2 = max_1
+        max_1 = current
+    if current < max_1 and current > max_2:
+        max_2 = current
+    return _recursive_find_2nd_largest(arg[1:], max_1, max_2)
+
+def recursive_find_2nd_largest(arg):
+    if len(arg) < 2:
+        return None
+    max_1 = max(arg[0], arg[1])
+    max_2 = min(arg[0], arg[1])
+    return _recursive_find_2nd_largest(arg[2:], max_1, max_2)
+
+arg =  [2,3,5,4]
+print(recursive_find_2nd_largest(arg))
+arg =  [2,5,4,3,5]
+print(recursive_find_2nd_largest(arg))
+arg =  [2,5]
+print(recursive_find_2nd_largest(arg))
