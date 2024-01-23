0a404549d346511064ffd5f85f42de67ba84865d
ivanakhmetev <78900410+ivanakhmetev@users.noreply.github.com>
2024-01-23 18:21:30 +0300

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,22 +1,22 @@
 # recursion task 7
 
-def _recursive_find_2nd_largest(arg, max_1, max_2):
-    if len(arg) == 0:
+def _recursive_find_2nd_largest(arg, max_1, max_2, i):
+    if len(arg[i:]) == 0:
         return max_2
-    current = arg[0]
+    current = arg[i]
     if current >= max_1:
         max_2 = max_1
         max_1 = current
     if current < max_1 and current > max_2:
         max_2 = current
-    return _recursive_find_2nd_largest(arg[1:], max_1, max_2)
+    return _recursive_find_2nd_largest(arg, max_1, max_2, i+1)
 
 def recursive_find_2nd_largest(arg):
     if len(arg) < 2:
         return None
     max_1 = max(arg[0], arg[1])
     max_2 = min(arg[0], arg[1])
-    return _recursive_find_2nd_largest(arg[2:], max_1, max_2)
+    return _recursive_find_2nd_largest(arg, max_1, max_2, 2)
 
 arg =  [2,3,5,4]
 print(recursive_find_2nd_largest(arg))
